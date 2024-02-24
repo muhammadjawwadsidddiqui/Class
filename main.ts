a6bc273ffeb067c4b01ecea74aa1f4461d616b6b
Muhammad jawwad siddiqui <160030111+muhammadjawwadsidddiqui@users.noreply.github.com>
2024-02-25 01:41:13 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,32 @@
+let stringtypeExample = "This is a string";
+console.log(stringtypeExample); 
+
+let numbertypeExample = 100;
+console.log(numbertypeExample);
+
+let datatyoeExample1 = true
+console.log(datatyoeExample1);
+
+let datatyoeExample2 = false
+console.log(datatyoeExample2);
+
+var massege = "1234";
+console.log(massege);
+
+var massegeNew = "5678";
+console.log(massegeNew);
+
+let favriotcolour ="yellow";
+console.log(favriotcolour);
+favriotcolour = "black"
+console.log(favriotcolour);
+
+const massegeNew1 ="123";
+console.log(massegeNew1);
+
+let userName: string = "Muhammad jawwad siddiqui";
+console.log(userName);
+let Age: Number = 20;
+console.log(Age);
+let muhammadjawwadsiddiqui: boolean = true;
+console.log(muhammadjawwadsiddiqui);
